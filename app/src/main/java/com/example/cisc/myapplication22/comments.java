f9c32bedab331530bb1032bac72d83a9e4165bb4
CISC Student <cisc@ac.sdmesa.net>
2017-09-21 12:20:29 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cisc/myapplication22/comments.java
@@ -0,0 +1,11 @@
+// Intents
+// Android Webview
+
+//log.txt -- a log file
+// it saves everything that the user do,everything single thing that happen .
+// a exception also gets written in the log file.
+// You can put severity levels at the log file.
+//
+//
+//
+
